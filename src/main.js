cb928f74e1fd0a4e4ba4560c0a36b8d68fa8751d
UltraBirdTech <ultrabirdrepti@gmail.com>
2021-05-24 19:23:04 +0900

    add some rogic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
-window.alert('Open Application')
+function buttonClick(text) {
+    window.alert(text);
+    console.log(text);
+}
+
+class YouTube {
+    constructor(){
+        this.name = 'YouTube';
+        this.button_id = '';
+        this.text_box_id = '';
+    }
+}
+
+class Discord {
+    constructor(){
+        this.name = 'Discord';
+        this.button_id = '';
+        this.text_box_id = '';
+    }
+}
+
+// add some class.
+
+function checkSite() {
+    console.log(location.hostname);
+    var hostname = location.hostname;
+    console.log(hostname);
+    if(hostname == 'www.youtube.com') {
+        console.log('This is YouTube')
+        return YouTube()
+    } else {
+        console.log('対応していません')
+    }
+}
+
+let site = checkSite();
+console.log(site.name);
+
+//let button = document.getElementById('');
+//button.onclick = ButtonClick('function JavaScript!!!');
